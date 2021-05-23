abb787c2215ea7196a9fcc821ef245b5fa6839b7
souravkar <sourav.kar@nagarro.com>
2021-05-23 13:14:10 +0530

    initial commit

---
+++ src/main/java/com/mvnwebapp/test/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.mvnwebapp.test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/welcome")
+public class HelloServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//super.doGet(req, resp);
+		resp.setContentType("text/plain");
+		resp.getWriter().write("Maven web project\n");
+		resp.getWriter().write("Deployed on Apache Tomcat server");
+	}
+
+}
